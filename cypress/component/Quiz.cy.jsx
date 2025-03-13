ea2680408ae247529416edc061963d6159b19417
Tanya <dougtanyah@gmail.com>
2025-03-13 17:46:15 -0400

    test/answering question gets next question

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -7,46 +7,38 @@ import { mount } from 'cypress/react18';
 describe('<Quiz />', ()=> {
 //before each test intercept API fetch requests, mock questions in quizData.json
 //use alias @getQuestions 
-  beforeEach(() => {
-    cy.intercept('GET', '/api/questions/random', {fixture: 'quizData.json'}).as('getQuestions');
-  });
+    beforeEach(() => {
+      cy.intercept('GET', '/api/questions/random', {fixture: 'quizData.json'}).as('getQuestions');
+    });
 
-//test render of start quiz button by looking for the text
-it('renders the Start Quiz button initially', ()=>{
-  mount(<Quiz />);
-  cy.contains('Start Quiz').should('be.visible');
+    //test render of start quiz button by looking for the text
+    it('renders the Start Quiz button initially', ()=>{
+       mount(<Quiz />);
+       cy.contains('Start Quiz').should('be.visible');
+    });
+
+    //test starting the quiz and getting first question
+    it('starts the quiz and gets the first question', ()=> {
+        mount(<Quiz />);
+        cy.contains('Start Quiz').click();
+        cy.wait('@getQuestions').its('response.body').then((quizData) => {
+        cy.contains(quizData[0].question).should('be.visible');
+        })
+    });
+
+    //test clicking an answer proceeds to next question
+    it('allows answering question and gets next question', () => {
+        mount (< Quiz />);
+        cy.contains('Start Quiz').click();
+        cy.wait('@getQuestions').its('response.body').then((quizData) => {
+        //click the correct answer
+        cy.contains('button', `${1}`).click();  // Clicking button 
+        //next question should be visible
+        cy.contains(quizData[1].question).should('be.visible');
+      });
+    });
+
+
 });
 
-//test starting the quiz and getting first question
-it('starts the quiz and gets the first question', ()=>{
-  mount(<Quiz />);
-  cy.contains('Start Quiz').click();
-  cy.wait('@getQuestions').its('response.body').then((quizData) => {
-    cy.contains(quizData[0].question).should('be.visible');
-    })
-});
-
-/*
-it('allows answering questions and proceeds to the next one', () => {
-  mount(<Quiz />);
-  cy.contains('Start Quiz').click();
-  cy.wait('@getQuestions');
-  
-  cy.contains(mockQuestions[0].answers[1].text).click(); // Click correct answer
-  cy.contains(mockQuestions[1].question).should('be.visible');
-});
-
-it('completes the quiz and shows the final score', () => {
-  mount(<Quiz />);
-  cy.contains('Start Quiz').click();
-  cy.wait('@getQuestions');
-
-  cy.contains(mockQuestions[0].answers[1].text).click();
-  cy.contains(mockQuestions[1].answers[1].text).click();
-  
-  cy.contains('Quiz Completed').should('be.visible');
-  cy.contains('Your score: 2/2').should('be.visible');
-});
-});
-*/
 
